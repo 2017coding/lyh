08b435161eda7fc9346d6c485a7b2af5ffbf4cd2
lyh <1695304848@qq.com>
2019-06-04 15:09:04 +0800

    布局

---
+++ src/views/home/components/Component3.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="component" :style="'background-image:' + background">
+    1
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    background: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.component{
+  padding: 20px;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,141 +1,110 @@
 <template>
   <div class="page-home">
-    <div class="bg" />
-    <div class="grid">
-      <div
-        v-for="(item, index) in gridList"
+    <div ref="box" class="box">
+      <component
+        :is="item.component"
+        v-for="(item, index) in componentsList"
         :key="index"
-        :style="item.style"
-        class="grid-item"
-      >
-        <render :render="item.render" :item="item" />
-      </div>
+        :background="item.background"
+      />
     </div>
+    <ul class="dot-box">
+      <li v-for="(item, index) in componentsList" :key="index">
+        <i
+          :style="{'background-image': index === Math.abs(addr) ? componentsList[index].background : ''}"
+          :class="{dot: true, 'dot-active': index === Math.abs(addr)}"
+          @click="handleClick('clickDot', -index)"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Render from './components/Render'
-// import sss from './components/sss'
-
-// 显示天气的方法， 要在这个位置初始化
-(function (T, h, i, n, k, P, a, g, e) {
-  g = function () {
-    P = h.createElement(i)
-    a = h.getElementsByTagName(i)[0]
-    P.src = k
-    P.charset = 'utf-8'
-    P.async = 1
-    a.parentNode.insertBefore(P, a)
-  }
-  T['ThinkPageWeatherWidgetObject'] = n
-  T[n] || (T[n] = function () { (T[n].q = T[n].q || []).push(arguments) })
-  T[n].l = +new Date()
-  if (T.attachEvent) {
-    T.attachEvent('onload', g)
-  } else {
-    T.addEventListener('load', g, false)
-  }
-}(window, document, 'script', 'tpwidget', '//widget.seniverse.com/widget/chameleon.js'))
+import Component1 from './components/Component1'
+import Component2 from './components/Component2'
+import Component3 from './components/Component3'
+import Component4 from './components/Component4'
 
 export default {
   components: {
-    Render
+    Component1,
+    Component2,
+    Component3,
+    Component4
   },
   data () {
     return {
-      gridList: [
-        // 天气
+      addr: 0,
+      flag: true,
+      componentsList: [
         {
-          style: 'grid-row: 1 / 2; grid-column: 11 / 12; background: rgba(0, 0, 0, .5)',
-          render: (h, data) => {
-            return (
-              <div
-                id='tp-weather-widget'
-                style='padding: 10px'
-              />
-            )
-          }
-        },
-        // 个人简介
-        {
-          style: 'grid-row: 2 / 6; grid-column: 1 / 3;',
-          render: (h, data) => {
-            return (
-              <div class='11'>个人简介</div>
-            )
-          }
-        },
-        // 掘金
-        {
-          style: 'grid-row: 2 / 4; grid-column: 10 / 12;',
-          render: (h, data) => {
-            return (
-              <div class='11'>掘金</div>
-            )
-          }
-        },
-        // 思否
-        {
-          style: 'grid-row: 4 / 6; grid-column: 10 / 11;',
-          render: (h, data) => {
-            return (
-              <div class='11'>思否</div>
-            )
-          }
-        },
-        // CSDN
-        {
-          style: 'grid-row: 4 / 5; grid-column: 11 / 12;',
-          render: (h, data) => {
-            return (
-              <div class='11'>CSDN</div>
-            )
-          }
-        },
-        // 音乐播放器
-        {
-          style: 'grid-row: 5 / 6; grid-column: 1 / 2;',
-          render: (h, data) => {
-            return (
-              <div class='11'>音乐播放器</div>
-            )
-          }
+          name: '',
+          component: 'Component1',
+          background: 'linear-gradient( 135deg, #F761A1 10%, #8C1BAB 100%)'
         },
         {
-          style: 'grid-row: 6 / 7; grid-column: 1 / 2;',
-          render: (h, data) => {
-            return (
-              <div class='11'>我</div>
-            )
-          }
+          name: '',
+          component: 'Component2',
+          background: 'linear-gradient( 135deg, #FFF6B7 10%, #F6416C 100%)'
+        },
+        {
+          name: '',
+          component: 'Component3',
+          background: 'linear-gradient( 135deg, #81FFEF 10%, #F067B4 100%)'
+        },
+        {
+          name: '',
+          component: 'Component4',
+          background: 'linear-gradient( 135deg, #ABDCFF 10%, #0396FF 100%)'
         }
       ]
     }
   },
   mounted () {
-    this.initWeather()
+    this.listenMousewheel()
   },
   methods: {
-    // 使用天气
-    initWeather () {
-      // 方法存在开始加载
-      if (typeof tpwidget === 'function') {
-        tpwidget('init', {
-          'flavor': 'slim',
-          'location': 'WX4FBXXFKE4F',
-          'geolocation': 'enabled',
-          'language': 'auto',
-          'unit': 'c',
-          'theme': 'chameleon',
-          'container': 'tp-weather-widget',
-          'bubble': 'enabled',
-          'alarmType': 'badge',
-          'color': '#C6C6C6',
-          'uid': 'UBBB8DBE07',
-          'hash': '6dda6603c46080d47f2c51d061c86bba'
-        })
-        tpwidget('show')
+    listenMousewheel () {
+      const box = this.$refs.box
+      document.onmousewheel = e => {
+        if (!this.flag) return
+        e = e || window.event
+        if (e.wheelDelta) { // 判断浏览器IE，谷歌滑轮事件
+          this.flag = false
+          if (e.wheelDelta > 0) { // 当滑轮向上滚动时
+            this.addr++
+          }
+          if (e.wheelDelta < 0) { // 当滑轮向下滚动时
+            this.addr--
+          }
+        } else if (e.detail) { // Firefox滑轮事件
+          this.flag = false
+          if (e.detail > 0) { // 当滑轮向上滚动时
+            this.addr++
+          }
+          if (e.detail < 0) { // 当滑轮向下滚动时
+            this.addr--
+          }
+        }
+        setTimeout(() => {
+          this.flag = true
+        }, 1000)
+        if (this.addr >= 0) {
+          this.addr = 0
+        } else if (-this.addr >= this.componentsList.length - 1) {
+          this.addr = -this.componentsList.length + 1
+        }
+        box.style.cssText = `transform: translate3d(0, ${this.addr * 100}%, 0); transition: all 1s`
+      }
+    },
+    handleClick (type, data) {
+      const box = this.$refs.box
+      switch (type) {
+        case 'clickDot':
+          this.addr = data
+          box.style.cssText = `transform: translate3d(0, ${this.addr * 100}%, 0); transition: all 1s`
+          break
       }
     }
   }
@@ -144,35 +113,26 @@ export default {
 
 <style lang="scss" scoped>
 .page-home{
-  padding: 20px;
   height: 100%;
-  min-width: 980px;
-  .bg{
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-image: linear-gradient( 135deg, #FFF6B7 10%, #F6416C 100%);
-  }
-  .grid{
+  overflow: hidden;
+  .box{
     height: 100%;
-    // 网格布局
-    display: grid;
-    // 定义列 auto-fit(自适应) fr(等分) minmax(最小宽度，最大宽度) // 最大宽度设置为12列
-    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
-    // 定义行 auto-fit(自动填充)
-    grid-template-rows: repeat(auto-fill, minmax(100px, 1fr));
-    // 定义item之间的间距为20px
-    grid-gap: 10px;
-    grid-auto-flow: column;
-    .grid-item{
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: relative;
-      background: rgba(0, 0, 0, .1);
-      border-radius: 2px;
+  }
+  .dot-box{
+    position: fixed;
+    right: 40px;
+    bottom: 20px;
+    .dot{
+      margin-bottom: 5px;
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      background: rgba(0, 0, 0, .2);
+      cursor: pointer;
+      &.dot-active{
+        opacity: 1;
+      }
     }
   }
 }
